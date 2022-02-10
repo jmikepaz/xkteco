3f9338a847f9114a6ffb8ee5960da5663d650e2c
Juan Paz <juan.paz@grupoemco.com>
2022-02-09 18:14:47 -0600

    libs

--- zkteco.js
+++ zkteco.js
@@ -1,20 +1,22 @@
 const ZKLib = require('zklib');
  
-ZK = new ZKLib({
+let zkInstance = new ZKLib({
   ip: '192.168.131.153',
   port: 4370,
   inport: 5200,
   timeout: 5000,
 });
  
-// connect to access control device
-ZK.connect(function(err) {
-  if (err) throw err;
-  console.log('Conetado');
- 
-  // read the time info from th device
-  ZK.getAttendance(function(err, data) {
-    if (err) throw err;
-    console.log( data );
-  });
-});
+try {
+    // Create socket to machine 
+    await zkInstance.createSocket()
+
+
+    // Get general info like logCapacity, user counts, logs count
+    // It's really useful to check the status of device 
+    console.log(await zkInstance.getInfo())
+} catch (e) {
+    console.log(e)
+    if (e.code === 'EADDRINUSE') {
+    }
+}
